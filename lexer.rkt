96e68efc191084f1f5f32aff4168dfeae74da10a
javad abdi <abdijavad110@gmail.com>
2020-06-01 22:57:51 +0430

    add lexer

---
+++ lexer.rkt
@@ -0,0 +1,55 @@
+#lang racket
+
+
+(require parser-tools/lex
+         (prefix-in : parser-tools/lex-sre)
+         parser-tools/yacc)
+
+(define our-lexer
+  (lexer
+   (whitespace (our-lexer input-port))
+   ((eof) (token-EOF))
+   ("while" (token-WHILE))
+   ("do" (token-DO))
+   ("end" (token-END))
+   ("if" (token-IF))
+   ("then" (token-THEN))
+   ("else" (token-ELSE))
+   ("endif" (token-ENDIF))
+   ("return" (token-RETURN))
+   ("null" (token-NULL))
+   ("true" (token-TRUE))
+   ("false" (token-FALSE))
+   (
+    (:: (:or (char-range #\a #\z) (char-range #\A #\Z))
+             (:* (:or (char-range #\a #\z) (char-range #\A #\Z) (char-range #\0 #\9) #\_ #\-)))
+    (token-VARIABLE (string->symbol lexeme)))
+   (
+    (:+ (char-range #\0 #\9))
+    (token-POS (string->number lexeme)))
+   (
+    (:: #\" (complement #\") #\")
+    (token-STRING lexeme))
+   ("," (token-COMMA))
+   (";" (token-SEMICOL))
+   ("=" (token-=))
+   ("<" (token-<))
+   (">" (token->))
+   ("==" (token-==))
+   ("!=" (token-!=))
+   ("(" (token-LPAR))
+   (")" (token-RPAR))
+   ("[" (token-LBRACKET))
+   ("]" (token-RBRACKET))
+   ("+" (token-+))
+   ("*" (token-*))
+   ("-" (token--))
+   ("/" (token-/))
+))
+
+(define-tokens a (VARIABLE POS STRING operator))
+(define-empty-tokens b (EOF SEMICOL COMMA = < > == != + - * / LPAR RPAR LBRACKET RBRACKET WHILE DO END IF THEN ELSE ENDIF RETURN NULL TRUE FALSE))
+
+;test
+(define lex-this (lambda (lexer input) (lambda () (lexer input))))
+(define my-lexer (lex-this our-lexer (open-input-string "while <>= == != ( ) [ ] a do b + 3; end")))
