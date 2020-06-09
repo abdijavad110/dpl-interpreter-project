56dc3023b6f66e45f14c8ebc9877a3cb73dd26dd
Soroosh Baselizadeh <soroosh.baselizadeh@gmail.com>
2020-06-09 22:26:55 +0430

    RETURN solved. May be better solutions. operator-helper fixed (especially for lists).

--- value-of.rkt
+++ value-of.rkt
@@ -48,7 +48,8 @@
   (lambda (com env)
     ;(display env)
     (cond
-      [(unitcom-expr? com) (display (unitcom-expr-ucom com)) (value-of-unitcom (unitcom-expr-ucom com) env)]
+      [RETURN RETVAL]
+      [(unitcom-expr? com) (value-of-unitcom (unitcom-expr-ucom com) env)] ;(display (unitcom-expr-ucom com)) 
       [(multi-command-expr? com) (begin
                                (value-of-command (multi-command-expr-mcom com) env)
                                (value-of-command (multi-command-expr-ucom com) env))])))
@@ -59,7 +60,7 @@
     [(whilecom-expr? ucom) (value-of-while-expr (whilecom-expr-whileexpr ucom) env)]
     [(ifcom-expr? ucom) (value-of-if-expr (ifcom-expr-ifexpr ucom) env)]
     [(assigncom-expr? ucom) (value-of-assign-expr (assigncom-expr-assignexpr ucom) env)]
-    [(returncom-expr? ucom) (begin (define a (value-of-return-expr (returncom-expr-returnexpr ucom) env)) (display (expval-value a)))])))
+    [(returncom-expr? ucom) (begin (define a (value-of-return-expr (returncom-expr-returnexpr ucom) env)) (set! RETVAL (make-return-value a)) (set! RETURN #t))])))
 
 (define value-of-while-expr
   (lambda (whileexpr x)
@@ -93,59 +94,71 @@
 
 (define <
   (lambda (a b)
-  (cond
-    [(or (null? a) (null? b)) #f]
-    [(and (string? a) (string? b)) (str-cmp `l a b)]  
-    [(and (list? a) (list? b)) (display "can not compare two lists")]
-    [(list? a)  (if (null? (cdr a)) (< (car a) b) (and (< (car a) b) (< (cdr a) b)))]
-    [(list? b)  (if (null? (cdr b)) (< a (car b)) (and (< a (car b)) (< a (cdr b))))]
-    [else (old< a b)]
-      )))
+    (begin
+    (cond [(expval? a) (set! a (expval-value a))])
+    (cond [(expval? b) (set! b (expval-value b))])
+    (cond
+      [(or (null? a) (null? b)) #f]
+      [(and (string? a) (string? b)) (str-cmp `l a b)]  
+      [(and (list? a) (list? b)) (display "can not compare two lists")]
+      [(list? a)  (if (null? (cdr a)) (< (car a) b) (and (< (car a) b) (< (cdr a) b)))]
+      [(list? b)  (if (null? (cdr b)) (< a (car b)) (and (< a (car b)) (< a (cdr b))))]
+      [else (old< a b)]
+      ))))
 
 (define >
   (lambda (a b)
-  (cond
-    [(or (null? a) (null? b)) #f]
-    [(and (string? a) (string? b)) (str-cmp `g a b)] 
-    [(and (list? a) (list? b)) (display "can not compare two lists")]
-    [(list? a)  (if (null? (cdr a)) (> (car a) b) (and (> (car a) b) (> (cdr a) b)))]
-    [(list? b)  (if (null? (cdr b)) (> a (car b)) (and (> a (car b)) (> a (cdr b))))]
-    [else (old> a b)]
-      )))
+    (begin
+    (cond [(expval? a) (set! a (expval-value a))])
+    (cond [(expval? b) (set! b (expval-value b))])
+    (cond
+      [(or (null? a) (null? b)) #f]
+      [(and (string? a) (string? b)) (str-cmp `g a b)] 
+      [(and (list? a) (list? b)) (display "can not compare two lists")]
+      [(list? a)  (if (null? (cdr a)) (> (car a) b) (and (> (car a) b) (> (cdr a) b)))]
+      [(list? b)  (if (null? (cdr b)) (> a (car b)) (and (> a (car b)) (> a (cdr b))))]
+      [else (old> a b)]
+      ))))
 
 (define =
   (lambda (a b)
-  (cond
-    [(and (null? a) (null? b)) #t]
-    [(and (list? a) (list? b)) (if (old= (length a) (length b)) (and (= (car a) (car b)) (= (cdr a) (cdr b))) #f)]
-    [(list? a)  (if (null? (cdr a)) (= (car a) b) (and (= (car a) b) (= (cdr a) b)))]
-    [(list? b)  (if (null? (cdr b)) (= a (car b)) (and (= a (car b)) (= a (cdr b))))]
-    [(or (null? a) (null? b)) #f]
-    [(and (string? a) (string? b)) (equal? a b)]
-    [(and (boolean? a) (boolean? b)) (not (xor a b))]
-    [ (or (and (boolean? a) (number? b)) (and (boolean? b) (number? a))
-          (and (string? a) (number? b)) (and (string? b) (number? a))
-          (and (string? a) (boolean? b)) (and (string? b) (boolean? a)))
-      #f]
-    [else (old= a b)]
-      )))
+    (begin
+    (cond [(expval? a) (set! a (expval-value a))])
+    (cond [(expval? b) (set! b (expval-value b))])
+    (cond
+      [(and (null? a) (null? b)) #t]
+      [(and (list? a) (list? b)) (if (old= (length a) (length b)) (and (= (car a) (car b)) (= (cdr a) (cdr b))) #f)]
+      [(list? a)  (if (null? (cdr a)) (= (expval-value (car a)) b) (and (= (expval-value (car a)) b) (= (cdr a) b)))]
+      [(list? b)  (if (null? (cdr b)) (= a (expval-value (car b))) (and (= a (expval-value (car b))) (= a (cdr b))))]
+      [(or (null? a) (null? b)) #f]
+      [(and (string? a) (string? b)) (equal? a b)]
+      [(and (boolean? a) (boolean? b)) (not (xor a b))]
+      [ (or (and (boolean? a) (number? b)) (and (boolean? b) (number? a))
+            (and (string? a) (number? b)) (and (string? b) (number? a))
+            (and (string? a) (boolean? b)) (and (string? b) (boolean? a)))
+        #f]
+      [else (old= a b)]
+      )))) 
 
 (define !=
   (lambda (a b)
-  (cond
-    [(and (null? a) (null? b)) #f]
-    [(and (string? a) (string? b)) (equal? a b)]
-    [(and (boolean? a) (boolean? b)) (xor a b)]
-    [(and (list? a) (list? b)) (if (old= (length a) (length b)) #t (and (!= (car a) (car b)) (!= (cdr a) (cdr b))))]
-    [(list? a)  (if (null? (cdr a)) (!= (car a) b) (or (!= (car a) b) (!= (cdr a) b)))]
-    [(list? b)  (if (null? (cdr b)) (!= a (car b)) (or (!= a (car b)) (!= a (cdr b))))]
-    [(or (null? a) (null? b)) #f]
-    [ (or (and (boolean? a) (number? b)) (and (boolean? b) (number? a))
-          (and (string? a) (number? b)) (and (string? b) (number? a))
-          (and (string? a) (boolean? b)) (and (string? b) (boolean? a)))
-      #t]
-    [else (not (old= a b))]
-      )))
+    (begin
+    (cond [(expval? a) (set! a (expval-value a))])
+    (cond [(expval? b) (set! b (expval-value b))])
+    (cond
+      [(and (null? a) (null? b)) #f]
+      [(and (string? a) (string? b)) (equal? a b)]
+      [(and (boolean? a) (boolean? b)) (xor a b)]
+      [(and (list? a) (list? b)) (if (old= (length a) (length b)) #t (and (!= (car a) (car b)) (!= (cdr a) (cdr b))))]
+      [(list? a)  (if (null? (cdr a)) (!= (car a) b) (or (!= (car a) b) (!= (cdr a) b)))]
+      [(list? b)  (if (null? (cdr b)) (!= a (car b)) (or (!= a (car b)) (!= a (cdr b))))]
+      [(or (null? a) (null? b)) #f]
+      [ (or (and (boolean? a) (number? b)) (and (boolean? b) (number? a))
+            (and (string? a) (number? b)) (and (string? b) (number? a))
+            (and (string? a) (boolean? b)) (and (string? b) (boolean? a)))
+        #t]
+      [else (not (old= a b))]
+      ))))
 
 
 ;; helpers:
@@ -164,30 +177,42 @@
 
 (define str-cmp
     (lambda (f a b)
-      (str-cmp-helper f (string->list a) (string->list b))))
+      (begin
+        (cond [(expval? a) (set! a (expval-value a))])
+        (cond [(expval? b) (set! b (expval-value b))])
+        (str-cmp-helper f (string->list a) (string->list b)))))
 
 
 (define neg-helper
     (lambda (a)
-     [(number? a) (int->expval (- a))]
-     [(boolean? a) (bool->expval (not a))]
-     [(list? a) (if (null? (cdr a)) (neg-helper (car a)) (cons (neg-helper (car a)) (neg-helper (cdr a))))]
-     ;[else (display "invalid argument after dash")] ;TODO
-     ))
+      (begin
+        (cond [(expval? a) (set! a (expval-value a))])
+        [(number? a) (int->expval (- a))]
+        [(boolean? a) (bool->expval (not a))]
+        [(list? a) (if (null? (cdr a)) (neg-helper (car a)) (cons (neg-helper (car a)) (neg-helper (cdr a))))]
+        ;[else (display "invalid argument after dash")] ;TODO
+        )))
 
 (define operator-helper
     (lambda (f a b)
-     (cond
-      [(and (number? a) (number? b)) (int->expval (f a b))]
-      [(and (boolean? a) (boolean? b)) (cond
-                                        [(equal? f +) (bool->expval (or a b))]
-                                        [(equal? f *) (bool->expval (and a b))]
-                                        [else (display "invalid operation between booleans")])]
-      [(and (string? a) (string? b) (equal? f +)) (string->expval (string-append a b))]
-      [(and (list? a) (list? b)) (append a b)]
-      [(list? a)  (if (null? (cdr a)) (f (car a) b) (cons (f (car a) b) (f (cdr a) b)))]
-      [(list? b)  (if (null? (cdr b)) (f a (car b)) (cons (f a (car b)) (f a (cdr b))))]
-      [else (display "invalid arguments for operator")])))
+      (begin
+        ;(display a)
+        ;(display b)
+        (cond [(expval? a) (set! a (expval-value a))])
+        (cond [(expval? b) (set! b (expval-value b))])
+      (cond
+        [(and (number? a) (number? b)) (int->expval (f a b))]
+        [(and (boolean? a) (boolean? b)) (cond
+                                           [(equal? f +) (bool->expval (or a b))]
+                                           [(equal? f *) (bool->expval (and a b))]
+                                           [else (display "invalid operation between booleans")])]
+        [(and (string? a) (string? b) (equal? f +)) (string->expval (string-append a b))]
+        [(and (list? a) (list? b)) (our-list-expval (append a b))]
+        [(list? a)  (if (null? (cdr a)) (our-list-expval (list (operator-helper f (car a) b))) (our-list-expval (cons (operator-helper f (car a) b) (let ([res (operator-helper f (cdr a) b)])
+                                                                                                                                                      (if (expval? res) (expval-value res) (res))))))]
+        [(list? b)  (if (null? (cdr b)) (our-list-expval (list (operator-helper f a (car b)))) (our-list-expval (cons (operator-helper f a (car b)) (let ([res (operator-helper f a (cdr b))])
+                                                                                                                                                      (if (expval? res) (expval-value res) (res))))))]
+        [else (display "invalid arguments for operator")]))))
 
 (define reference-helper
     (lambda (l idx)
@@ -269,12 +294,24 @@
       [(idx-expr? lm) (racket-list->expval (list (value-of-expression (listmem-exp1 lm) env)))]
       [(multi-idx-expr? lm) (racket-list->expval (cons (value-of-expression (listmem-exp1 lm) env) (value-of-listmem (multi-idx-expr-lm lm) env)))])))
 
+(define make-return-value
+  (lambda (x)
+    (begin
+    (cond
+      [(expval? x) (set! x (expval-value x))])
+    (cond
+      [(list? x) (if (null? x) '() (cons (make-return-value (car x)) (make-return-value (cdr x))))]
+      [else x]))))
+      ;TODO NULL and STRING
+
 ;test
 (define lex-this (lambda (lexer input) (lambda () (lexer input))))
-(define my-lexer (lex-this our-lexer (open-input-string "x = [1, 2, 3, 4, 5]; return x[0]; if x[4] == 3 then a = [6] else a = [17, 10, 2] endif; while a[1] > 1 do b = a[0]-1; a=[b, a[1]-1, 10, 2] end; return b")))
+(define my-lexer (lex-this our-lexer (open-input-string "x = [1, 2, 3, 4, 5]; y = [1, 2, 3, 4, 5]; return x+y; if x != y then a = 4*[6, 10, 2]/2 else a = [17, 10, 2] endif; while a[1] > 1 do b = a[0]-1; a=[b, a[1]-1, 10, 2] end; return b")))
 ;)))
 ;"a = 2; while a < 4 do a = a + 1; b = 3 end; return a"
 ;(my-lexer)(my-lexer)(my-lexer)(my-lexer)(my-lexer)(my-lexer)(my-lexer)(my-lexer)(my-lexer)(my-lexer)(my-lexer)(my-lexer)(my-lexer)(my-lexer)(my-lexer)(my-lexer)(my-lexer)(my-lexer)
 ;(define env (empty-env))
+(define RETURN #f)
+(define RETVAL '())
 (let ((parser-res (our-parser my-lexer))) (value-of-command parser-res env)) ;(parse-object-to-list parser-res))
 ;(cdr (list 1))
